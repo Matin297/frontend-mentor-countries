c7c90611ed5a9df5a94ad988f5ae242ed1cba98c
Matin Mir <asmamir297@gmail.com>
2023-11-24 15:35:28 +0330

    add the ability to filter countries upon loading

--- services/countries.js
+++ services/countries.js
@@ -1,6 +1,14 @@
 import { fetchCountries } from "./api.js";
+import { debounce } from "./utility.js";
 
-export async function loadCountries() {
-  const countries = await fetchCountries();
+export async function loadCountries(filterCb) {
+  let countries = await fetchCountries();
+
+  if (filterCb) {
+    countries = countries.filter(filterCb);
+  }
+
   countries_app.store.countries = countries;
 }
+
+export const debouncedLoadCountries = debounce(loadCountries);
