dc6674652b8c00371e89f1a57e52bb3aaf5c70e5
Matin Mir <asmamir297@gmail.com>
2023-11-24 13:18:18 +0330

    implement country item component

--- components/CountryItem.js
+++ components/CountryItem.js
@@ -2,4 +2,27 @@ export default class CountryItem extends HTMLElement {
   constructor() {
     super();
   }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    const template = document.getElementById("country-item-template");
+    const content = template.content.cloneNode(true);
+
+    this.append(content);
+
+    const country = JSON.parse(this.dataset.country);
+
+    this.querySelector("a").addEventListener("click", (event) => {
+      event.preventDefault();
+    });
+
+    this.querySelector("img").src = country.flag;
+    this.querySelector("h2").textContent = country.name;
+    this.querySelector(".country__population").textContent = country.population;
+    this.querySelector(".country__region").textContent = country.region;
+    this.querySelector(".country__capital").textContent = country.capital;
+  }
 }
