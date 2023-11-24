957a9e7ae882359c1478f67d02c45f48b994d47b
Matin Mir <asmamir297@gmail.com>
2023-11-24 11:40:26 +0330

    assemble countries page component without data

--- components/CountriesPage.js
+++ components/CountriesPage.js
@@ -1,5 +1,46 @@
+import { fetchStyles } from "../services/api.js";
+
 export default class CountriesPage extends HTMLElement {
   constructor() {
     super();
+
+    this.attachShadow({ mode: "open" });
+
+    let style = document.createElement("style");
+
+    fetchStyles("/", "styles").then((styles) => {
+      style.textContent = styles;
+    });
+
+    const search = document.createElement("country-search");
+    const filter = document.createElement("country-filter");
+
+    this.shadowRoot.append(style, search, filter);
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    let list;
+    const countries = countries_app.store.countries;
+
+    if (!countries) {
+      list = document.createElement("p");
+      list.textContent = "No countries yet!";
+    } else {
+      list = document.createElement("ul");
+
+      const listItems = countries.map((country) => {
+        const item = document.createElement("li");
+        item.dataset.country = JSON.stringify(country);
+        return item;
+      });
+
+      list.append(...listItems);
+    }
+
+    this.shadowRoot.append(list);
   }
 }
