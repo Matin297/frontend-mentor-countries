92dfb618c5da1fd54ee84bc96dc83ef7925d227e
Matin Mir <asmamir297@gmail.com>
2023-11-24 12:05:34 +0330

    add proxy to store to broadcast countries change

--- services/store.js
+++ services/store.js
@@ -2,4 +2,14 @@ const STORE = {
   countries: null,
 };
 
-export default STORE;
+const STORE_PROXY = new Proxy(STORE, {
+  set(target, property, value) {
+    target[property] = value;
+    if (property === "countries") {
+      window.dispatchEvent(new CustomEvent("countrieschange"));
+    }
+    return true;
+  },
+});
+
+export default STORE_PROXY;
