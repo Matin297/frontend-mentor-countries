9d43eb76558d8ea2edd88a92cbe88fb2bbcb3fb9
Matin Mir <asmamir297@gmail.com>
2023-11-24 11:39:19 +0330

    add a utility for fetching css files

--- services/api.js
+++ services/api.js
@@ -2,6 +2,11 @@ const URL = {
   countries: "/data.json",
 };
 
+export async function fetchStyles(path, filename) {
+  const response = await fetch(`${path}${filename}.css`);
+  return await response.text();
+}
+
 export async function fetchCountries() {
   const response = await fetch(URL.countries);
   return await response.json();
