03123d7af37c6e79b6f804f5b44e363f0905df81
Matin Mir <asmamir297@gmail.com>
2023-11-24 15:03:08 +0330

    implement country search component

--- components/CountrySearch.js
+++ components/CountrySearch.js
@@ -2,4 +2,14 @@ export default class CountrySearch extends HTMLElement {
   constructor() {
     super();
   }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    const template = document.getElementById("country-search-template");
+    const content = template.content.cloneNode(true);
+    this.append(content);
+  }
 }
