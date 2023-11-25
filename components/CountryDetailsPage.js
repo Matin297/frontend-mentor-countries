5c6725624af346e56003616ac2c3a1af1d598359
Matin Mir <asmamir297@gmail.com>
2023-11-25 11:30:14 +0330

    details: attach shadow dom and read template

--- components/CountryDetailsPage.js
+++ components/CountryDetailsPage.js
@@ -1,5 +1,12 @@
 export default class CountryDetailsPage extends HTMLElement {
   constructor() {
     super();
+
+    this.attachShadow({ mode: "open" });
+
+    const template = document.getElementById("country-details-template");
+    const content = template.content.cloneNode(true);
+
+    this.shadowRoot.append(content);
   }
 }
