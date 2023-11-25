41bbf98da8c327c544939aafad4078e9393d432b
Matin Mir <asmamir297@gmail.com>
2023-11-25 10:18:59 +0330

    add filter by region functionality

--- components/CountryFilter.js
+++ components/CountryFilter.js
@@ -1,3 +1,5 @@
+import { loadCountries } from "../services/countries.js";
+
 export default class CountryFilter extends HTMLElement {
   constructor() {
     super();
@@ -11,5 +13,10 @@ export default class CountryFilter extends HTMLElement {
     const template = document.getElementById("country-filter-template");
     const content = template.content.cloneNode(true);
     this.append(content);
+
+    this.querySelector("select").addEventListener("change", (event) => {
+      const value = event.target.value;
+      loadCountries((country) => country.region.toLowerCase().includes(value));
+    });
   }
 }
