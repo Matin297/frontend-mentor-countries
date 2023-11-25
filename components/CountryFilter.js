5879811f0b634f8e6948c7160980f542d8b741c6
Matin Mir <asmamir297@gmail.com>
2023-11-25 10:06:57 +0330

    implement filter skeleton and styles

--- components/CountryFilter.js
+++ components/CountryFilter.js
@@ -2,4 +2,14 @@ export default class CountryFilter extends HTMLElement {
   constructor() {
     super();
   }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    const template = document.getElementById("country-filter-template");
+    const content = template.content.cloneNode(true);
+    this.append(content);
+  }
 }
